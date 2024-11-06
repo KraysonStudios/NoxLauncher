7d1a053f9cc0ef92b5f6652d2df3d12cfe462cd8
DevCheckOG <ing.kevin.benavides@gmail.com>
2024-11-06 15:11:37 -0600

    Tiny Changes

--- build/windows.sh
+++ build/windows.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-wine "/home/linuxu/.wine/drive_c/Program Files/Python312/Scripts/pyinstaller.exe" --clean --name="NoxLauncher" --optimize=2 --nowindowed --icon="../assets/icon.ico" --target-architecture=x86_64 --workpath="./work" --distpath="./windows" "../main.py"
-=======
 wine "/home/linuxu/.wine/drive_c/Program Files/Python312/Scripts/pyinstaller.exe" --clean --name="NoxLauncher" --optimize=2 --noconsole --icon="../assets/icon.ico" --target-architecture=x86_64 --workpath="./work" --distpath="./windows" "../main.py"
->>>>>>> 53edf80 (Initialization of the new Edition of Nox Launcher)
