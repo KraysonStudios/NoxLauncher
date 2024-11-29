836db4b4b7a8315e72e58586979a65a310f645ea
DevCheckOG <ing.kevin.benavides@gmail.com>
2024-11-29 17:52:39 -0600

    ThreadPool Changes

--- threadpool.py
+++ threadpool.py
@@ -1,3 +1,3 @@
 from concurrent.futures import ThreadPoolExecutor
 
-NOXLAUNCHER_THREADPOOL: ThreadPoolExecutor = ThreadPoolExecutor(max_workers= 20, thread_name_prefix= "NoxLauncher Background")
+NOXLAUNCHER_THREADPOOL: ThreadPoolExecutor = ThreadPoolExecutor(max_workers= 5, thread_name_prefix= "NoxLauncher Background")
