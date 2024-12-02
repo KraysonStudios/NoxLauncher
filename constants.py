891ad65d76167d2e5946ae7ed2439ea5f0d44c41
DevCheckOG <ing.kevin.benavides@gmail.com>
2024-12-02 06:07:04 -0600

    Windows Identify Fix

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 DEPLOYMENT_TYPE: str = "BETA"
-VERSION: str = "0.6.1"
+VERSION: str = "0.6.2"
 DISCORD_CLIENT_ID: str = "1264755410695491605"
 AZURE_APP_ID: str = "698e9da9-0859-4f92-8fc6-c147ce7467c5"
 AZURE_APP_URL_REDIRECT: str = "https://login.microsoftonline.com/common/oauth2/nativeclient"
